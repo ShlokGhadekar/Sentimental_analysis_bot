a331ccd31d21e6179413b1ceea420a525100eea3
ShlokGhadekar <145497918+ShlokGhadekar@users.noreply.github.com>
2025-05-28 11:43:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,14 @@ import requests
 import os
 
 app = FastAPI()
-
+@app.get("/")
+def read_root():
+    return {"message": "Sentimental Analysis Bot is live!"}
+    
+@app.get("/favicon.ico")
+def favicon():
+    return {"message": "No favicon"}
+    
 API_URL = "https://api-inference.huggingface.co/models/distilbert-base-uncased-finetuned-sst-2-english"
 API_TOKEN = os.getenv("HF_TOKEN")  # store this securely
 
